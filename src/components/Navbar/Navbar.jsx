e5072a0867f5ffebde51e95613c5133ce52bd441
Siar Wahidi <siar.wahidi@gmail.com>
2024-07-02 17:26:04 -0500

    cleanup along with initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <section className="container">
+      <div>
+        <p>Kuya Joe's Kitchen</p>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
