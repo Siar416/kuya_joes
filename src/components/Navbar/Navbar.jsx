491efe08813c423bcfb122cfe6b73891a6765793
Siar Wahidi <siar.wahidi@gmail.com>
2024-07-03 17:34:39 -0500

    Added the menu items along with some styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
+
   return (
     <section className="container">
-      <div>
+      <div className="logo">
         <p>Kuya Joe's Kitchen</p>
       </div>
+      {menuOpen ? (
+        <div className="nav">
+          <ul className="nav-items">
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      ) : (
+        <div className="menu-icon" onClick={handleClick}>
+          <FiMenu />
+        </div>
+      )}
     </section>
   );
 };
